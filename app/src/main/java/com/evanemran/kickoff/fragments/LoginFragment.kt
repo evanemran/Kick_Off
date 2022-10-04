bf36385f2674776099e9e0ce26a0056ee897b6d8
Emran Khandaker Evan <evanemran007@gmail.com>
2022-10-04 16:25:39 +0600

    auth page added

---
+++ app/src/main/java/com/evanemran/kickoff/fragments/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.evanemran.kickoff.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.evanemran.kickoff.R
+
+class LoginFragment : Fragment() {
+
+//    var view: View? = View(context)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        view = inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
